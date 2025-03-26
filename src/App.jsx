e8aa6300be5911dc44a1c3b4abd2f72267a51d87
Mazhar <mazharmohal03@gmail.com>
2025-03-26 12:11:55 +0500

    UPDATED:UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const [trendingMovies, setTrendingMovies] = useState([]);
+  // const [trendingMovies, setTrendingMovies] = useState([]);
   // debounce the search term to prevent many API request
   // by waiting for the user to stop typing  for 500ms
   useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
